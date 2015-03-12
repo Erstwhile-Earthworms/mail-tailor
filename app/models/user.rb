cd86da0d23cd39a78aa3a8b5b7b8451e25710184
cfmeyers <cfmeyers@gmail.com>
2015-03-12 05:57:17 -0400

    initial user model tests, user name/email unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:email) }
+  it { should be_valid }
+
+  describe "when name not present" do
+    before { @user.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when email not present" do
+    before { @user.email = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when name is already taken" do
+    before do
+      user_with_same_name = @user.dup
+      user_with_same_name.save
+    end
+    it { should_not be_valid }
+  end
+
 end
