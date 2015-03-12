59e6eb0d6500c7ea013cae10bea6637cb4c9e0b7
cfmeyers <cfmeyers@gmail.com>
2015-03-12 08:15:22 -0400

    add user signup form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,6 +9,31 @@ RSpec.describe "UserPages", :type => :request do
     it { is_expected.to have_content('Sign up') }
   end
 
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with:"Example User"
+        fill_in "Email", with:"user@example.com"
+        fill_in "Password", with:"foobar"
+        fill_in "Password confirmation", with:"foobar"
+      end
+
+      it "should create a new user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
+  end
+
   describe "individual user page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
