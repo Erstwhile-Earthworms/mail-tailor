bc13b2e922250d5c0d46e29dad2226a646e4c128
cfmeyers <cfmeyers@gmail.com>
2015-03-12 06:25:13 -0400

    add validators for user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 
+  has_secure_password
 end
