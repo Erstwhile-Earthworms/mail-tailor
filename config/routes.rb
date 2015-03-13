b757f1cc483b86c0adf238e4305f363f413e2eae
cfmeyers <cfmeyers@gmail.com>
2015-03-13 01:17:48 -0400

    add sessions for user sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root "static_pages#home"
   match '/signup', to: 'users#new', via: 'get'
-  resources :users
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,6 +30,12 @@ RSpec.describe "UserPages", :type => :request do
       it "should create a new user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+        it { should have_link('Sign out')}
+      end
     end
 
   end
