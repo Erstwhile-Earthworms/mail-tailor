abace3e904839de24c58feadad7e9619966de4bb
cfmeyers <cfmeyers@gmail.com>
2015-03-18 23:35:10 -0400

    update specs to reflect new login/logout/signup

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,31 +4,28 @@ RSpec.describe "Authentication", :type => :request do
 
   describe "signin page" do
     subject { page }
-    before { visit signin_path }
-
-    it { should have_content('Sign in') }
-
-    describe "with invalid information" do
-      before { click_button 'Sign in' }
-
-      it { should have_content('Sign in') }
+    before do 
+      visit root_path 
+      find('#login').click
     end
+
+    it { should have_content('LOGIN') }
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
+        find('#session_email').set(user.email.upcase)
+        find('#session_password').set(user.password)
         click_button "Sign in"
       end
 
-      it { should have_link('Sign out',    href: signout_path) }
+      it { should have_link('LOGOUT',    href: signout_path) }
 
       describe "followed by signout" do
-        before { click_link 'Sign out' }
-        it { should have_link 'Sign in' }
-        
+        before { click_link 'LOGOUT' }
+        it { should have_content('LOGIN') }
       end
+
     end
 
     
