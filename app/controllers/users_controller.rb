35b898504c74da4d47acc07d90eb4163ef4ba408
cfmeyers <cfmeyers@gmail.com>
2015-03-20 12:07:51 -0400

    fixed @senders instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @senders = @user.letters.select(:sender).distinct
+    @senders = @user.letters.map { |letter| letter.sender }
   end
 
   private
